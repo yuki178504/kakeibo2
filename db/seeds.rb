0354b20cf466392d8b212400eb30d8ef5904ded4
yuki178504 <aerosmit1989x@gmail.com>
2023-02-13 01:34:49 +0900

    [add]bootStrap導入 showアクションのルーティング追加、コントローラーにアクション追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Book.create(inout: 1, category: "給料", year: 2020, month: 7, amount: 30)
+Book.create(inout: 2, category: "家賃", year: 2020, month: 7, amount: 8)
+Book.create(inout: 2, category: "食費", year: 2020, month: 7, amount: 6)
+Book.create(inout: 2, category: "光熱費", year: 2020, month: 7, amount: 3)
+Book.create(inout: 2, category: "保険", year: 2020, month: 7, amount: 2)
