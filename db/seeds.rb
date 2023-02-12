45869d642cdeaee01cb9c831a58cb16ea3f27ebd
yuki178504 <aerosmit1989x@gmail.com>
2023-02-13 00:33:06 +0900

    [add]一覧表示のビュー、コントローラー、テストデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create(inout: 1, category: "給料", year: 2020, month: 7, amount: 30)
